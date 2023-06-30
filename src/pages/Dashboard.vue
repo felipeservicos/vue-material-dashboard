458cda201a7be486755c6f60d47f5e5b9a680cb1
fr22497 <fr22497@furnas.com.br>
2023-06-30 17:19:58 -0300

    -Added Calculate button with method

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="content">
     <div class="md-layout">
-       <div
+      <div
         class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100"
       >
         <stats-card data-background-color="green">
@@ -181,7 +181,21 @@
             >
           </md-card-header>
           <md-card-content>
-            <ordered-table table-header-color="orange"></ordered-table>
+            <div>
+              <!-- <md-table v-model="users" :table-header-color="tableHeaderColor">
+                <md-table-row slot="md-table-row" slot-scope="{ item }">
+                  <md-table-cell md-label="ID">{{ item.id }}</md-table-cell>
+                  <md-table-cell md-label="Name">{{ item.name }}</md-table-cell>
+                  <md-table-cell md-label="Salary">{{
+                    item.salary
+                  }}</md-table-cell>
+                  <md-table-cell md-label="Country">{{
+                    item.country
+                  }}</md-table-cell>
+                  <md-table-cell md-label="City">{{ item.city }}</md-table-cell>
+                </md-table-row>
+              </md-table> -->
+            </div>
           </md-card-content>
         </md-card>
       </div>
@@ -192,17 +206,12 @@
 <script>
 import axios from "axios";
 
-import {
-  StatsCard,
-  ChartCard,
-  OrderedTable,
-} from "@/components";
+import { StatsCard, ChartCard } from "@/components";
 
 export default {
   components: {
     StatsCard,
     ChartCard,
-    OrderedTable,
   },
   mounted() {},
   methods: {
@@ -242,13 +251,38 @@ export default {
           this.minimalSamplesChart.data.labels = res.data.map((t) => t.model);
           this.minimalSamplesChart.data.series = b;
           this.minimalSamplesChart.options.high = Math.max(a) + 10;
-
-          this.emailsSubscriptionChart.data.labels = res.data.map(
-            (t) => t.model
-          );
-          this.emailsSubscriptionChart.data.series = b;
-          this.emailsSubscriptionChart.options.high = Math.max(a) + 10;
           this.showChart = true;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    sendRelationship() {
+      if (this.isConvolutionalApplication) {
+        this.input.features = this.input.height * this.input.width;
+      }
+      axios
+        .get(
+          "http://localhost:8080/lowerBoundsSamplesBetweenAccuracyAndReliability?features=" +
+            this.input.features +
+            "&neurons=" +
+            this.input.neurons +
+            "&layers=" +
+            this.input.layers +
+            "&model=" +
+            this.input.model +
+            "&range=" +
+            this.input.range
+        )
+        .then((res) => {
+          this.lowerBoundsSamplesTable = res.data;
+
+          console.log("Headers LowerBounds Array:");
+          this.headerslowerBounds = [...Array(100 - this.input.range).keys()];
+          console.log(this.headerslowerBounds);
+          console.log("LowerBounds Array:");
+
+          console.log(res.data);
         })
         .catch((error) => {
           console.log(error);
@@ -260,20 +294,20 @@ export default {
       isConvolutionalApplication: false,
       simulationData: [],
       showChart: false,
+      lowerBoundsSamplesTable: [],
+      headerslowerBounds: [],
       input: {
         features: 10,
         neurons: 5,
         layers: 5,
         accuracy: 0.99,
         reliability: 0.99,
-        range: 0,
+        range: 20,
         height: 0,
         width: 0,
       },
       minimalSamplesChart: {
         data: {
-          // labels: ["12am", "3pm", "6pm", "9pm", "12pm", "3am", "6am", "9am"],
-          // series: [[230, 750, 450, 300, 280, 240, 200, 190]],
           labels: [],
           series: [[]],
         },
@@ -291,100 +325,6 @@ export default {
             left: 0,
           },
         },
-        responsiveOptions: [
-          [
-            "screen and (max-width: 640px)",
-            {
-              seriesBarDistance: 5,
-              axisX: {
-                labelInterpolationFnc: function (value) {
-                  return value[0];
-                },
-              },
-            },
-          ],
-        ],
-      },
-
-      dailySalesChart: {
-        data: {
-          labels: ["M", "T", "W", "T", "F", "S", "S"],
-          series: [[12, 17, 7, 17, 23, 18, 38]],
-        },
-        options: {
-          axisX: {
-            showGrid: false,
-          },
-          low: 0,
-          high: 1000,
-          chartPadding: {
-            top: 0,
-            right: 5,
-            bottom: 0,
-            left: 0,
-          },
-        },
-        responsiveOptions: [
-          [
-            "screen and (max-width: 640px)",
-            {
-              seriesBarDistance: 5,
-              axisX: {
-                labelInterpolationFnc: function (value) {
-                  return value[0];
-                },
-              },
-            },
-          ],
-        ],
-      },
-
-      emailsSubscriptionChart: {
-        data: {
-          labels: [
-            "Ja",
-            "Fe",
-            "Ma",
-            "Ap",
-            "Mai",
-            "Ju",
-            "Jul",
-            "Au",
-            "Se",
-            "Oc",
-            "No",
-            "De",
-          ],
-          series: [
-            [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
-          ],
-        },
-        options: {
-          axisX: {
-            showGrid: false,
-          },
-          low: 0,
-          high: 1000,
-          chartPadding: {
-            top: 0,
-            right: 5,
-            bottom: 0,
-            left: 0,
-          },
-        },
-        responsiveOptions: [
-          [
-            "screen and (max-width: 640px)",
-            {
-              seriesBarDistance: 5,
-              axisX: {
-                labelInterpolationFnc: function (value) {
-                  return value[0];
-                },
-              },
-            },
-          ],
-        ],
       },
     };
   },
